5347f81754417663b86c1d75fd21fa4b734cf844
Rohan Bansal <rohan@gatech.edu>
2012-04-25 18:49:06 -0400

    Updated code to use send instead of add API on Pocket

--- import.py
+++ import.py
@@ -6,6 +6,7 @@ import.py
 
 import sys
 import json
+import urllib
 import urllib2
 
 username = sys.argv[1]
@@ -15,9 +16,19 @@ jsonfile = sys.argv[4]
 
 json_data = open(jsonfile)
 data = json.load(json_data)
-
-for y in data:
-	url = 'https://readitlaterlist.com/v2/add?username=' + username + '&password='+ password + '&apikey=' + apikey + '&url=' + y['article__url']
-	response = urllib2.urlopen(url)
-	
 json_data.close()
+
+url_data = [dict(url=x['article__url']) for x in data]
+
+url_dict = dict(zip(xrange(len(url_data)), url_data))
+
+dump = json.dumps(url_dict)
+
+url = 'https://readitlaterlist.com/v2/send'
+values = {'username' : username, 'password' : password, 'apikey' : apikey, 'new' : dump}
+
+variables = urllib.urlencode(values)
+req = urllib2.Request(url, variables)
+response = urllib2.urlopen(req)
+print response.read()
+	
